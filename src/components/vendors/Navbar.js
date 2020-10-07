8435d8f8cb124b9ef1da4ce306eae268fbb997cb
Natalia Beckstead <toosmiravilla@gmail.com>
2020-10-06 20:50:51 -0600

    added stylings for nav bar

--- src/components/vendors/Navbar.js
+++ src/components/vendors/Navbar.js
@@ -1,19 +1,27 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import logo from "../../images/logo.png";
 
 function Navbar() {
 
-  return (
-    <div>
-        <div>
-            <Link className="link" to={`/submit_item`}>Submit item</Link>
-            <Link className="link" to={`/submit_promotion`}>Submit promotion</Link>
-            <Link className="link" to={`/submissions`}>See my submissions</Link>
-            <Link className="link" to={`/profile`}>Manage profile</Link>
+    let history = useHistory();
+
+    function handSignOut() {
+        history.push("/signin");
+    }
+
+    return (
+        <div className="navbar">
+            <img src={logo} alt="Harmons logo" />
+            <div>
+                <Link className="link" to={`/submit_item`}>Submit item</Link>
+                <Link className="link" to={`/submit_promotion`}>Submit promotion</Link>
+                <Link className="link" to={`/submissions`}>See my submissions</Link>
+                <Link className="link" to={`/profile`}>Manage profile</Link>
+            </div>
+            <button onClick={handSignOut}>Sign out</button>
         </div>
-        <button>Sign out</button>
-    </div>
-  )
+    )
 }
 
 export default Navbar;
