e14420515b8f4bdc08ff4e7250e218ccca31419b
Natalia Beckstead <toosmiravilla@gmail.com>
2020-10-08 17:40:23 -0600

    can click between pages in merch menu, basic stylings

--- src/components/vendors/MerchandiserMenu.js
+++ src/components/vendors/MerchandiserMenu.js
@@ -7,9 +7,23 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 function MerchandiserMenu() {
 
   const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1); //setting 1 to show fisrt page
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
+    setPageNumber(1);
+  }
+
+  function changePage(offset) {
+    setPageNumber(prevPageNumber => prevPageNumber + offset);
+  }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
   }
 
 
@@ -17,19 +31,31 @@ function MerchandiserMenu() {
     <>
         <Navbar />
         <div className="pdf-container" >
-          <Document 
+          <Document
+            className="pdf-doc"
             file={menuPDF}
             options={{ workerSrc: "/pdf.worker.js" }}
             onLoadSuccess={onDocumentLoadSuccess}
-            >
-            {Array.from(new Array(numPages), (el, index) => (
-            <Page className="pdf-file" key={`page_${index + 1}`} pageNumber={index + 1} />
-            ))}
+          >
+            <Page className="pdf-page" pageNumber={pageNumber} height="600" />
           </Document>
+          <div className="pdf-nav" >
+            <button type="button" disabled={pageNumber <= 1} onClick={previousPage}>◄</button>
+            <p>Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}</p>
+            <button type="button" disabled={pageNumber >= numPages} onClick={nextPage} >►</button>
+          </div>
         </div>
+        
     </>
   )
   
 }
 
 export default MerchandiserMenu
+
+// react-pdf documentation:
+// https://github.com/wojtekmaj/react-pdf
+
+// TODO:
+// add functionality to change size
+// add opportunity to download file
